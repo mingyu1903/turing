3371587fd58e180c5eaaba4146a078db8799f8c7
潘延杰 <panyanjie@bytedance.com>
2020-12-13 14:39:24 +0800

    feat: done

--- turing_M2.py
+++ turing_M2.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-W = 'aaabbcccccc#'
+W = 'aabbbcccccc#'
 
 # 转换字符串为数组方便操作(分别用1,2,3,4,9代替a,b,c,d,#,并用0代替x)
 w = [int(chr(ord(i)-48)) for i in W[:len(W)-1]]
@@ -107,7 +107,7 @@ def step3(w, i):
             return i, 1 
         elif s==2 or s==4:
             i -= 1 # 指针左移
-            return i, -1 # 此时只剩下一个a,进行下一步
+            return i, -2 # 此时只剩下一个a,进行下一步
         else:
             i += 1 # 指针右移
             return i, 0 # 返回指针位置和转移函数qrject(记为q[0]),拒绝
@@ -189,7 +189,7 @@ def step3(w, i):
     q = [qr, q1, q2, q3, q4, q5]
 
     i, t = qs(i)
-    while t!=-1 and t!=0:
+    while t!=-2 and t!=-1 and t!=0:
         i, t = q[t](i)
         # print(t)
         # print(w)
@@ -197,10 +197,83 @@ def step3(w, i):
     if t==0:
         qr()
     else:
-        print(t)
+        print(t, i)
 
-def step4(w, i):
+    print(w)
+
+def step4(w, i, t):
+
+    def qs1(i):
+        i += 1
+        return i, 0
+
+    def qs(i):
+        s = w[i]
+        if s==2 or s==4:
+            w[i] = 0
+            i += 1
+            return i, 1
+        else:
+            i += 1
+            return i, 0
+
+    def q1(i):
+        s = w[i]
+        if s==3:
+            w[i] = 0
+            i -= 1
+            return i, 2
+        elif s==0 or s==2 or s==4:
+            i += 1
+            return i, 1
+        else:
+            i += 1
+            return i, 4
+
+    def q2(i):
+        s = w[i]
+        if s==7:
+            i += 1
+            return i, 3
+        elif s==2 or s==4:
+            w[i] = 0
+            i += 1
+            return i, 1
+        elif s==0 or s==3:
+            i -= 1
+            return i, 2
+
+    def q3(i):
+        s = w[i]
+        if s==3:
+            i += 1
+            return i, 4
+        elif s==9:
+            i -= 1
+            return i, 5
+        else:
+            i += 1
+            return i, 3
+
+    def qr():
+        print('Reject')
+
+    def qa():
+        print('Accept')
+
+    q = [qs, q1, q2, q3, qr, qa]
+
+    if t==-2:
+        i, t = qs1(i)
+    elif t==-1:
+        i, t = qs(i)
+
+    while t!=4 and t!=5:
+        i, t = q[t](i)
+        # print('i=', i, 't=', t)
+        # print(w)
     
+    q[t]()
 
 
 
@@ -209,6 +282,8 @@ if __name__ == '__main__':
     # i, t = step1(w)
     # step2(w, i)
     step3(w, 1)
+    # print(w)
+    step4(w, 1, -1)
     # W = 'aaaabbbcc#' # 0*7
     # step1(W)
     # W = 'aabc#' # 0*8
